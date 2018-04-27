7dcaf0010111865a6edb53254771de4b1c029de8
CallumGooding <callum.gooding11@gmail.com>
2018-04-27 14:59:53 +0100

    27/04 b

--- src/CompanyEmailTest.java
+++ src/CompanyEmailTest.java
@@ -1,19 +1,5 @@
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.runner.RunWith;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.Assert.*;
-
-@RunWith(Arquillian.class)
-public class CompanyEmailTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(. class)
-  .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
+class CompanyEmailTest {
 
 }
