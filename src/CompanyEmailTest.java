0debb3e5d536af8d8d14a9b9aebd093d952800f8
CallumGooding <callum.gooding11@gmail.com>
2018-04-27 15:37:03 +0100

    27/04 e

--- src/CompanyEmailTest.java
+++ src/CompanyEmailTest.java
@@ -1,14 +1,21 @@
 import org.junit.*;
+import static org.junit.Assert.*;
 
 // Authors:
 //      - Callum Gooding
 //      - Omar Aziz
-class CompanyEmailTest {
+public class CompanyEmailTest{
     private CompanyEmail blankEmail;
     private CompanyEmail populatedEmail;
     @Before
     public void setup(){
         blankEmail = new CompanyEmail();
         populatedEmail = new CompanyEmail("psyoba@nottingham.ac.uk", "psycg2@nottingham.ac.uk","Test email", "Test msg");
+    }
+
+    @Test
+    public void fromTest(){
+        String fromAddress = populatedEmail.fromAddress();
+        assertEquals(fromAddress, "psyoba@nottingham.ac.uk");
     }
 }
