7e7a3ecf6c5dcc1967155ad5bcbc9f8685334074
CallumGooding <callum.gooding11@gmail.com>
2018-04-27 15:27:23 +0100

    27/04 d

--- src/CompanyEmailTest.java
+++ src/CompanyEmailTest.java
@@ -1,7 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.*;
+
 // Authors:
 //      - Callum Gooding
 //      - Omar Aziz
 class CompanyEmailTest {
-
+    private CompanyEmail blankEmail;
+    private CompanyEmail populatedEmail;
+    @Before
+    public void setup(){
+        blankEmail = new CompanyEmail();
+        populatedEmail = new CompanyEmail("psyoba@nottingham.ac.uk", "psycg2@nottingham.ac.uk","Test email", "Test msg");
+    }
 }
