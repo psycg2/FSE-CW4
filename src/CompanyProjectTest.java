22b69236720599c9e296f50550afaf919c9e6fef
Alexis <psyaa12@nottingham.ac.uk>
2018-05-01 14:51:50 +0100

    test companyproject class (initial)

---
+++ src/CompanyProjectTest.java
@@ -0,0 +1,18 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+// Authors:
+// Alexandros Araouzos
+// Antontis Kourris
+// Christiana Efstratiou
+
+public class CompanyProjectTest {
+
+    private CompanyProject newContact;
+
+    @Before
+    public void setup(){
+        newContact = new CompanyProject();
+    }
+}
+
