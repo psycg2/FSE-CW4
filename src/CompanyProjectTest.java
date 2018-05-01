acc494deea548048e2e331db6f1917605b9486c4
Alexis <psyaa12@nottingham.ac.uk>
2018-05-01 17:58:40 +0100

    test companyproject class (initial)

--- src/CompanyProjectTest.java
+++ src/CompanyProjectTest.java
@@ -2,23 +2,34 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 // Authors:
-// Alexandros Araouzos
-// Antontis Kourris
-// Christiana Efstratiou
+//  Alexandros Araouzos
+//  Antontis Kourris
+//  Christiana Efstratiou
 
 public class CompanyProjectTest {
 
     private CompanyProject newProject;
+    private CompanyProject newID;
 
     @Before
     public void setup(){
-        newProject = new CompanyProject("Alex");
+        newProject = new CompanyProject("First Project");
+        newID = new CompanyProject("1");
     }
 
     @Test
-    public void projectIDTest(){
-        String ProjectTitle = newProject.getPTitle();
-        assertEquals(ProjectTitle, "Alex");
+    public void ProjectIdTest(){
+        int PhaseID = newProject.getPID();
+        assertEquals(PhaseID, 1);
+        int PhaseID2 = PhaseID+1;
+        assertEquals(PhaseID2, 2);
     }
+
+    @Test
+    public void ProjectTitleTest(){
+        String ProjectTitle = newProject.getPTitle();
+        assertEquals(ProjectTitle, "First Project");
+    }
+
 }
 
